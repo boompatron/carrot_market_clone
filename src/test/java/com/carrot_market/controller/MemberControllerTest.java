4f31e213da7c1fbb56aa7e8f918abc222520f3d8
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-05-25 22:08:10 +0900

    220525_2_177_login_test

---
+++ src/test/java/com/carrot_market/controller/MemberControllerTest.java
@@ -0,0 +1,52 @@
+package com.carrot_market.controller;
+
+import com.carrot_market.dto.MemberFormDto;
+import com.carrot_market.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import com.carrot_market.entity.Member;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class MemberControllerTest {
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(String email, String password){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail(email);
+        memberFormDto.setAddress("address");
+        memberFormDto.setPassword(password);
+        memberFormDto.setName("Test Name");
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        return memberService.saveMember(member);
+    }
+
+    @Test
+    @DisplayName("Login Success Test")
+    public void loginSuccessTest() throws Exception{
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+        mockMvc.perform(formLogin().userParameter("email").loginProcessingUrl("/members/login")
+                .user(email).password(password)).andExpect(SecurityMockMvcResultMatchers.authenticated());
+    }
+}
