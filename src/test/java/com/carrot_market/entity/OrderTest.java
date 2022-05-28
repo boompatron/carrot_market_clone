25deaeb928f55bcad129126e42b166a28b4a0e0f
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-05-28 15:49:50 +0900

    220528_3_209_cascade_test

---
+++ src/test/java/com/carrot_market/entity/OrderTest.java
@@ -0,0 +1,68 @@
+package com.carrot_market.entity;
+
+import com.carrot_market.constant.ItemSellStatus;
+import com.carrot_market.repository.ItemRepository;
+import com.carrot_market.repository.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@Transactional
+public class OrderTest {
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Item createItem(){
+        Item item = new Item();
+        item.setItemNm("test name");
+        item.setPrice(10000);
+        item.setItemDetail("test item detail");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        return item;
+    }
+
+    @Test
+    @DisplayName("cascade tess")
+    public void cascadeTest(){
+        Order order = new Order();
+
+        for(int i = 1; i <= 3; i++){
+            Item item = this.createItem();
+            itemRepository.save(item);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(20);
+            orderItem.setOrderPrice(1000);
+            orderItem.setOrder(order);
+            order.getOrderItems().add(orderItem);
+        }
+
+        orderRepository.saveAndFlush(order);
+        em.clear();
+
+        Order savedOrder = orderRepository.findById(order.getId()).orElseThrow(EntityNotFoundException::new);
+        assertEquals(3, savedOrder.getOrderItems().size());
+    }
+
+}
